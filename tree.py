731f04b175220962d9f0e8996ce9da0806213f8a
Cade <johnston2@grinnell.edu>
2025-06-11 10:41:57 -0500

    Added colored distribution methods and AAC class

--- tree.py
+++ tree.py
@@ -3,6 +3,30 @@ import random
 import matplotlib.pyplot as plot
 import matplotlib.colors as color
 
+class AAC:
+    arr = []
+    def increment(self, key):
+        for p in self.arr:
+            if p[0] == key:
+                p[1] += 1
+                return
+        self.arr.append([key, 1])
+    def get_val(self, key):
+        for p in self.arr:
+            if p[0] == key:
+                return p[1]
+        return 0
+    def get_keys(self):
+        output = []
+        for p in self.arr:
+            output.append(p[0])
+        return output
+    def get_max_val(self):
+        output = 0
+        for p in self.arr:
+            output = max(p[1],output)
+        return output
+    
 class node:
     loc = [0, 0]
     children = []
@@ -51,12 +75,40 @@ class node:
         else:
             arr[0].pop()
             arr[1].pop()
+    def gen_dist_helper(self, aac):
+        if self.children == []:
+            aac.increment(self.loc)
+        else:
+            for child in self.children:
+                child.gen_dist_helper(aac)
+
+    def gen_dist(self):
+        arr = AAC()
+        self.gen_dist_helper(arr)
+        keys = arr.get_keys()
+        max = arr.get_max_val()
+        xArr = []
+        yArr = []
+        cArr = []
+        for k in keys:
+            xArr.append(k[0])
+            yArr.append(k[1])
+            cArr.append(color.hsv_to_rgb(((arr.get_val(k)/max)*5/6,1,1)))
+        plot.scatter(xArr, yArr, c=cArr)
 
 root = node([0,0], 0)
 # print("Running")
 for i in range(10):
     # print("Running with i =",i)
     root.propogate(i)
-root.plot_path([[],[]])
+# root.plot_path([[],[]])
+plot.axis([-10, 10, -10, 10])
+plot.grid(True)
+ticks = []
+for i in range(21):
+    ticks.append(i-10)
+plot.xticks(ticks)
+plot.yticks(ticks)
+root.gen_dist()
 plot.show()
 # print("Terminating")
