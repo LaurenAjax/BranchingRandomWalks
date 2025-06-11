0b113bfdf646b8ce4eeec8873b859e3370f7e0fc
Cade <johnston2@grinnell.edu>
2025-06-11 10:43:49 -0500

    Updated spacing

--- tree.py
+++ tree.py
@@ -11,16 +11,19 @@ class AAC:
                 p[1] += 1
                 return
         self.arr.append([key, 1])
+        
     def get_val(self, key):
         for p in self.arr:
             if p[0] == key:
                 return p[1]
         return 0
+    
     def get_keys(self):
         output = []
         for p in self.arr:
             output.append(p[0])
         return output
+    
     def get_max_val(self):
         output = 0
         for p in self.arr:
@@ -31,29 +34,35 @@ class node:
     loc = [0, 0]
     children = []
     parent = 0
+    
     def __init__(self, pos, par):
         self.loc = pos.copy()
         # for val in pos:
         #     loc.append(val + 0)
         self.children = []
         self.parent = par
+        
     def add_child(self, child):
         self.children.append(child)
+        
     def wander_me(self):
         val = random.randint(0, 3)
         delta = [val%2*pow(-1,math.floor(val/2)), (val+1)%2*pow(-1,math.floor(val/2))]
         for i in range(2):
             self.loc[i] += delta[i]
         # print("(",self.loc[0],", ",self.loc[1],")")
+    
     def wander(self,num):
         if num == 1:
             self.wander_me()
         else:
             for child in self.children:
                 child.wander(num - 1)
+    
     def propogate_me(self):
         for i in range(2):
             self.add_child(node(self.loc, self))
+    
     def propogate(self, num):
         if num == 0:
             # print("Propogating self")
@@ -64,6 +73,7 @@ class node:
             for child in self.children:
                 # print("Propogating child with num = ",num - 1)
                 child.propogate(num - 1)
+    
     def plot_path(self, arr):
         arr[0].append(self.loc[0])
         arr[1].append(self.loc[1])
@@ -75,6 +85,7 @@ class node:
         else:
             arr[0].pop()
             arr[1].pop()
+    
     def gen_dist_helper(self, aac):
         if self.children == []:
             aac.increment(self.loc)
