b38dda67d2eb9003c07ee93a816c8edf2bd16214
Lauren Ajax <laurenajax@Laurens-MacBook-Air.local>
2025-06-13 22:40:13 -0500

    Done

--- TestBranchingRandomWalk.py
+++ TestBranchingRandomWalk.py
@@ -3,22 +3,23 @@ import copy
 import matplotlib.pyplot as plot
 
 class Node:
-    def __init__(self, parent, x_coord, y_coord, num_kids):
+    def __init__(self, parent, x_coord, y_coord, num_kids, gen):
         self.x_coord = x_coord
         self.y_coord = y_coord
         self.parent = None
         self.num_kids = num_kids
         self.next = []
+        self.gen = gen
         
 
 x = [0]
 y = [0]
-root = Node(None, x, y, random.randint(0, 2))
+root = Node(None, x, y, random.randint(0, 5), 1)
 
 def brancher(prev_node):
-    if (prev_node.num_kids == 0):
-        plot.plot(prev_node.x_coord, prev_node.y_coord, color=(random.random(),random.random(),random.random()))
-        plot.plot(prev_node.x_coord[len(prev_node.x_coord) - 1], prev_node.y_coord[len(prev_node.y_coord) - 1], 'o', color='k')
+    if (prev_node.num_kids == 0 or prev_node.gen == 10):
+        plot.plot(prev_node.x_coord, prev_node.y_coord, color=(0, 0, 0, 0.3))
+        plot.plot(prev_node.x_coord[len(prev_node.x_coord) - 1], prev_node.y_coord[len(prev_node.y_coord) - 1], 'o', color=(0, 0, 0, 0.3))
         return None
     else:
         x_cur = copy.deepcopy(prev_node.x_coord)
@@ -36,11 +37,12 @@ def brancher(prev_node):
         else:
             x_cur.append(x_cur[len(x_cur) - 1] - 1)
             y_cur.append(y_cur[len(y_cur) - 1])
-        cur_node = Node(prev_node, x_cur, y_cur, random.randint(0, 2))
+        cur_node = Node(prev_node, x_cur, y_cur, random.randint(0, 5), prev_node.gen + 1)
         for i in range(prev_node.num_kids):
             cur_node.next.append(brancher(cur_node))
         return cur_node
 
-root.next = brancher(root)
+for i in range(root.num_kids):
+    root.next.append(brancher(root))
 
 plot.show()
--- TestPolarWalk.py
+++ TestPolarWalk.py
@@ -12,10 +12,7 @@ class Node:
         self.next = []
         self.gen = gen
 
-
-theta = 0
-r = 0
-root = Node(None, theta, r, random.randint(0, 5), 0)
+root = Node(None, 0, 0, random.randint(0, 4), 0)
 lst = [root]
 count = 0
 
@@ -23,23 +20,28 @@ while count < len(lst):
     for i in range(lst[count].num_kids):
         random_value = random.randint(1, 22)
         if random_value < 8:
-            random_degree = random.randint(-10, 10)
+            cur_theta = math.radians(random.randint(-10, 10))
         elif random_value < 15:
-            random_degree = random.randint(110, 130)
+            cur_theta = math.radians(random.randint(110, 130))
         elif random_value < 22:
-            random_degree = random.randint(230, 250)
+            cur_theta = math.radians(random.randint(230, 250))
         else:
-            random_degree = random.randint(1, 360)
+            cur_theta = math.radians(random.randint(1, 360))
+        prev_r = lst[count].r_coord
+        prev_theta = lst[count].theta_coord
+        cur_r = math.sqrt((prev_r * math.cos(prev_theta) - math.cos(cur_theta))**2 + (prev_r * math.sin(prev_theta) - math.cos(cur_theta))**2)
         cur_gen = lst[count].gen + 1
         if cur_gen == 10:
-            cur_node = Node(lst[count], random_degree, lst[count].r_coord + 1, 0, cur_gen)
+            cur_node = Node(lst[count], cur_theta, cur_r, 0, cur_gen)
         else:
-            cur_node = Node(lst[count], random_degree, lst[count].r_coord + 1, random.randint(0, 5), cur_gen)
+            cur_node = Node(lst[count], cur_theta, cur_r, random.randint(0, 4), cur_gen)
         lst[count].next.append(cur_node)
         lst.append(cur_node)
     count += 1
 
 for node in lst:
-    plot.polar(math.radians(node.theta_coord), node.r_coord, 'o', color=(0, 0, 0, 0.3))
+    plot.polar(node.theta_coord, node.r_coord, 'o', color=(0, 0, 0, 0.3))
+    for kid in node.next:
+        plot.polar([node.theta_coord, kid.theta_coord], [node.r_coord, kid.r_coord], color=(0, 0, 0, 0.1))
 
 plot.show()
