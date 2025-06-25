23cb5a5cc9d1bd68497e3b7deaf306f33d1b626f
Lauren Ajax <laurenajax@Laurens-MacBook-Air.local>
2025-06-25 11:38:04 -0500

    Done

--- FamilyCloudModel.py
+++ FamilyCloudModel.py
@@ -46,8 +46,8 @@ while count < len(lst):
         if i == 0 and node.parent == None:
             angle = random.randint(1, 360)
         elif i == 0:
-            node.upper_bound = node.angle + 15
-            node.lower_bound = node.angle - 15
+            node.upper_bound = node.angle + 90
+            node.lower_bound = node.angle - 90
             angle = random.randint(node.lower_bound, node.upper_bound)
         elif node.parent == None:
             angle = random.randint(node.next[0].angle - 5, node.next[0].angle + 5)
@@ -58,15 +58,39 @@ while count < len(lst):
                 bottom = node.lower_bound
             if top > node.upper_bound:
                 top = node.upper_bound
+            if top == node.upper_bound and top <= bottom:
+                bottom = top - 5
+            if bottom == node.lower_bound and bottom >= top:
+                top = bottom + 5
             angle = random.randint(bottom, top)
-        for grandparent in node.parent.parent.next:
-            if grandparent is not node.parent:
-                for parent
+        if node.parent != None:
+            if node.parent.parent != None:
+                for grandparent in node.parent.parent.next:
+                    if grandparent is not node.parent:
+                        for auncle in grandparent.next:
+                            if angle < auncle.upper_bound and angle > auncle.lower_bound:
+                                up = auncle.upper_bound - angle
+                                down = angle - auncle.lower_bound
+                                if up > down:
+                                    angle = angle + up + random.randint(1, 5)
+                                else:
+                                    angle = angle - down - random.randint(1, 5)
+                            for cousin in auncle.next:
+                                node_x = prev_x + steps * math.cos(math.radians(angle))
+                                node_y = prev_y + steps * math.sin(math.radians(angle))
+                                cousin_x = cousin.r_coord * math.cos(cousin.theta_coord)
+                                cousin_y = cousin.r_coord * math.sin(cousin.theta_coord)
+                                distance = math.sqrt((node_x - cousin_x)**2 + (node_y - cousin_y)**2)
+                                if distance < 2:
+                                    if steps < 4:
+                                        steps += 1
+                                    else:
+                                        steps -= 1
         cur_x = prev_x + steps * math.cos(math.radians(angle))
         cur_y = prev_y + steps * math.sin(math.radians(angle))
         cur_r = math.sqrt(cur_x**2 + cur_y**2)
         cur_theta = math.atan2(cur_y, cur_x)
-        if cur_gen == 10:
+        if cur_gen == 8:
             cur_node = Node(node, angle, cur_theta, cur_r, 0, steps, cur_gen)
         elif cur_r > 5:
             cur_node = Node(node, angle, cur_theta, cur_r, 2, steps, cur_gen)
@@ -78,9 +102,8 @@ while count < len(lst):
     count += 1
 
 for node in lst:
-    plt.polar(node.theta_coord, node.r_coord, 'o', color=(0, 0, 0, 0.01))
     for kid in node.next:
-        plt.polar([node.theta_coord, kid.theta_coord], [node.r_coord, kid.r_coord], color=(random.random(), random.random(), random.random(), 0.5))
+        plt.polar([node.theta_coord, kid.theta_coord], [node.r_coord, kid.r_coord], color=(random.random(), random.random(), random.random(), 0.1))
 
 plt.show()
 # displays the plot
