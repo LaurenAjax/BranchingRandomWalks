ebcf7ce2fa448259a53e2240c4716b8d36ef3970
Lauren Ajax <laurenajax@Laurens-MacBook-Air.local>
2025-06-28 21:57:29 -0500

    Done

--- CousinRepulsionModel.py
+++ CousinRepulsionModel.py
@@ -28,7 +28,7 @@ def generate_node(parent_node, lower_bound, upper_bound):
     angle = random.randint(lower_bound, upper_bound)
     kid_x = parent_node.x_coord + math.cos(angle)
     kid_y = parent_node.y_coord + math.sin(angle)
-    if parent_node.gen < 29:
+    if parent_node.gen < 19:
         return Node(parent_node, angle, kid_x, kid_y, 2, parent_node.gen + 1)
     else:
         return Node(parent_node, angle, kid_x, kid_y, 0, parent_node.gen + 1)
@@ -39,48 +39,50 @@ def change_node_position(node):
     node.y_coord = node.parent.y_coord + math.sin(node.angle)
 
 def build_gen(cur_gen):
-    next_gen = []
-    for parent_node in cur_gen:
-        if parent_node.gen < 10:
-            for i in range(parent_node.num_kids):
-                kid_node = generate_node(parent_node, 1, 360)
-                parent_node.next.append(kid_node)
-                next_gen.append(kid_node)
-        else:
-            firstborn = []
-            for i in range(parent_node.num_kids):
-                if len(next_gen) == 0 and i == 0:
-                    kid_node = generate_node(parent_node, parent_node.angle - 85, parent_node.angle + 85)
+    if cur_gen[0].num_kids != 0:
+        next_gen = []
+        for parent_node in cur_gen:
+            if parent_node.gen < 10:
+                for i in range(parent_node.num_kids):
+                    kid_node = generate_node(parent_node, 1, 360)
                     parent_node.next.append(kid_node)
                     next_gen.append(kid_node)
-                    firstborn.append(kid_node)
-                elif i == 0:
-                    angle = random.randint(parent_node.angle - 85, parent_node.angle + 85)
-                    kid_x = parent_node.x_coord + math.cos(angle)
-                    kid_y = parent_node.y_coord + math.sin(angle)
-                    if parent_node.gen < 29:
-                        kid_node = Node(parent_node, angle, kid_x, kid_y, 2, parent_node.gen + 1)
-                    else:
-                        kid_node = Node(parent_node, angle, kid_x, kid_y, 0, parent_node.gen + 1)
-                    for cousin_node in firstborn:
-                        distance = math.sqrt((kid_node.x_coord - cousin_node.x_coord)**2 + (kid_node.y_coord - cousin_node.y_coord)**2)
-                        if distance < 0.9**removed(kid_node, cousin):
-                            change_node_position(kid_node)
-                    loop = 0
-                    while (kid_x != kid_node.x_coord or kid_y != kid_node.y_coord) and loop < 5:
-                        kid_x = kid_node.x_coord
-                        kid_y = kid_node.y_coord
+            else:
+                firstborn = []
+                for i in range(parent_node.num_kids):
+                    if len(next_gen) == 0 and i == 0:
+                        kid_node = generate_node(parent_node, parent_node.angle - 85, parent_node.angle + 85)
+                        parent_node.next.append(kid_node)
+                        next_gen.append(kid_node)
+                        firstborn.append(kid_node)
+                    elif i == 0:
+                        angle = random.randint(parent_node.angle - 85, parent_node.angle + 85)
+                        kid_x = parent_node.x_coord + math.cos(angle)
+                        kid_y = parent_node.y_coord + math.sin(angle)
+                        if parent_node.gen < 19:
+                            kid_node = Node(parent_node, angle, kid_x, kid_y, 2, parent_node.gen + 1)
+                        else:
+                            kid_node = Node(parent_node, angle, kid_x, kid_y, 0, parent_node.gen + 1)
                         for cousin_node in firstborn:
                             distance = math.sqrt((kid_node.x_coord - cousin_node.x_coord)**2 + (kid_node.y_coord - cousin_node.y_coord)**2)
                             if distance < 0.9**removed(kid_node, cousin):
                                 change_node_position(kid_node)
-                    parent_node.next.append(kid_node)
-                    next_gen.append(kid_node)
-                    firstborn.append(kid_node)
-                else:
-                    kid_node = generate_node(parent_node, parent_node.next[0].angle - 5, parent_node.next[0].angle + 5)
-                    parent_node.next.append(kid_node)
-                    next_gen.append(kid_node)
-    build_gen(next_gen)
+                        loop = 0
+                        while (kid_x != kid_node.x_coord or kid_y != kid_node.y_coord) and loop < 5:
+                            kid_x = kid_node.x_coord
+                            kid_y = kid_node.y_coord
+                            for cousin_node in firstborn:
+                                distance = math.sqrt((kid_node.x_coord - cousin_node.x_coord)**2 + (kid_node.y_coord - cousin_node.y_coord)**2)
+                                if distance < 0.9**removed(kid_node, cousin):
+                                    change_node_position(kid_node)
+                        parent_node.next.append(kid_node)
+                        next_gen.append(kid_node)
+                        firstborn.append(kid_node)
+                    else:
+                        kid_node = generate_node(parent_node, parent_node.next[0].angle - 5, parent_node.next[0].angle + 5)
+                        parent_node.next.append(kid_node)
+                        next_gen.append(kid_node)
+        build_gen(next_gen)
 
 build_gen([root])
+print("Model Built!")
