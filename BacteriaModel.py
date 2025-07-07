ea7ba066afc77e62874cc200bc7c072ced44252a
Lauren Ajax <laurenajax@Laurens-MacBook-Air.local>
2025-07-06 21:52:01 -0500

    Done

---
+++ BacteriaModel.py
@@ -0,0 +1,147 @@
+import matplotlib.pyplot as plt
+import random
+import math
+
+class Node:
+    def __init__(self, parent, angle, x_coord, y_coord, num_kids, gen):
+        self.parent = parent
+        self.angle = angle
+        self.x_coord = x_coord
+        self.y_coord = y_coord
+        self.num_kids = num_kids
+        self.next = []
+        self.gen = gen
+
+    def build_plot(self):
+        plt.plot(self.x_coord, self.y_coord, 'o', color = (0, 0, 1, 0.1))
+        x_array = [self.x_coord]
+        y_array = [self.y_coord]
+        cur_parent = self.parent
+        while cur_parent is not None:
+            x_array.append(cur_parent.x_coord)
+            y_array.append(cur_parent.y_coord)
+            cur_parent = cur_parent.parent
+        plt.plot(x_array, y_array, color = (0, 0, 0, 0.1))
+
+    def removed(self, other):
+        if(self.gen != other.gen):
+            return None
+        first_parent = self.parent
+        second_parent = other.parent
+        while first_parent is not second_parent:
+            first_parent = first_parent.parent
+            second_parent = second_parent.parent
+        distance = self.gen - first_parent.gen - 1
+        if distance >= 10:
+            return 10
+        else:
+            return distance
+
+    def density(self, x_coord_list, y_coordlist, beta):
+        total = 0
+        count = 0
+        for x, y in zip(x_coord_list, y_coord_list):
+            distance = math.sqrt((self.x_coord - x)**2 + (self.y_coord - y)**2)
+            if distance < beta:
+                count += 1
+            if distance < 2 * beta:
+                total += 1
+        if total == 0:
+            return 0
+        else:
+            return count / total
+    
+    def change_node_position(self, new_angle):
+        self.angle = new_angle
+        self.x_coord = self.parent.x_coord + math.cos(math.radians(self.angle))
+        self.y_coord = self.parent.y_coord + math.sin(math.radians(self.angle))
+
+    def change_num_kids(self, count):
+        self.num_kids = count
+
+root = Node(None, 0, 0, 0, 2, 0)
+x_coord_list = [0]
+y_coord_list = [0]
+alpha = 0.5
+beta = 1 - alpha
+
+def build_dot_plot():
+    plt.plot(x_coord_list, y_coord_list, 'o', color = (0, 0, 1, 0.1))
+
+def generate_node(parent_node, parent_x, parent_y, parent_gen, lower_bound, upper_bound):
+    angle = random.randint(lower_bound, upper_bound)
+    x = parent_x + math.cos(math.radians(angle))
+    y = parent_y + math.sin(math.radians(angle))
+    if parent_gen < 9:
+        return Node(parent_node, angle, x, y, 2, parent_gen + 1)
+    else:
+        return Node(parent_node, angle, x, y, 0, parent_gen + 1)
+
+def build_gen(cur_gen):
+    if cur_gen[0].num_kids != 0:
+        next_gen = []
+        firstborn = []
+        for parent_node in cur_gen:
+            parent_angle = parent_node.angle
+            parent_x = parent_node.x_coord
+            parent_y = parent_node.y_coord
+            parent_num_kids = parent_node.num_kids
+            parent_next = parent_node.next
+            parent_gen = parent_node.gen
+            upper_bound = parent_angle + 85
+            lower_bound = parent_angle - 85
+            upper_bound_x = parent_x + math.cos(math.radians(upper_bound))
+            upper_bound_y = parent_y + math.cos(math.radians(upper_bound))
+            lower_bound_x = parent_x + math.cos(math.radians(lower_bound))
+            lower_bound_y = parent_y + math.cos(math.radians(lower_bound))
+            if parent_gen < 4:
+                for i in range(parent_node.num_kids):
+                    kid_node = generate_node(parent_node, parent_x, parent_y, parent_gen, 1, 360)
+                    x_coord_list.append(kid_node.x_coord)
+                    y_coord_list.append(kid_node.y_coord)
+                    parent_next.append(kid_node)
+                    next_gen.append(kid_node)
+            else:
+                for i in range(parent_num_kids):
+                    kid_node = generate_node(parent_node, parent_x, parent_y, parent_gen, lower_bound, upper_bound)
+                    for cousin_node in firstborn:
+                        repelled = (10 - kid_node.removed(cousin_node))
+                        if math.sqrt((cousin_node.x_coord - kid_node.x_coord)**2 + (cousin_node.y_coord - kid_node.y_coord)**2) < repelled * alpha:
+                            if math.sqrt((cousin_node.x_coord - upper_bound_x)**2 + (cousin_node.y_coord - upper_bound_y)**2) > math.sqrt((cousin_node.x_coord - lower_bound_x)**2 + (cousin_node.y_coord - lower_bound_y)**2):
+                                if upper_bound > kid_node.angle + repelled * 2:
+                                    kid_node.change_node_position(kid_node.angle + repelled * 2)
+                                else:
+                                    kid_node.change_node_position(upper_bound)
+                            else: 
+                                if lower_bound < kid_node.angle - repelled * 2:
+                                    kid_node.change_node_position(kid_node.angle - repelled * 2)
+                                else:
+                                    kid_node.change_node_position(lower_bound)
+                    repelled = 5 * int(4 * kid_node.density(x_coord_list, y_coord_list, beta))
+                    if upper_bound - kid_node.angle > kid_node.angle - lower_bound:
+                        if upper_bound > kid_node.angle + repelled:
+                            kid_node.change_node_position(kid_node.angle + repelled)
+                        else: 
+                            kid_node.change_node_position(upper_bound)
+                    else:
+                        if lower_bound < kid_node.angle - repelled:
+                            kid_node.change_node_position(kid_node.angle - repelled)
+                        else: 
+                            kid_node.change_node_position(lower_bound)
+                    if kid_node.num_kids != 0:
+                        kid_node.change_num_kids(2 + int(4 * kid_node.density(x_coord_list, y_coord_list, beta)))
+                    x_coord_list.append(kid_node.x_coord)
+                    y_coord_list.append(kid_node.y_coord)
+                    parent_next.append(kid_node)
+                    next_gen.append(kid_node)
+                    if i == 0:
+                        firstborn.append(kid_node)
+        print("Gen " + str(next_gen[0].gen) + " Built!")
+        build_gen(next_gen)
+    else:
+        for node in cur_gen:
+            node.build_plot()
+
+build_gen([root])
+
+plt.show()
--- DensityRepulsionModel.py
+++ DensityRepulsionModel.py
@@ -3,7 +3,7 @@ import random
 import math
 
 class Node:
-    def __init__(self, parent, angle, x_coord, y_coord, steps, num_kids, gen):
+    def __init__(self, parent, angle, x_coord, y_coord, num_kids, gen):
         self.parent = parent
         # the parent node
         self.angle = angle
@@ -12,7 +12,6 @@ class Node:
         # the node's position on the x-axis
         self.y_coord = y_coord
         # the node's position on the y-axis
-        self.steps = steps
         self.num_kids = num_kids
         # the number of kids this node will have
         self.next = []
@@ -23,6 +22,22 @@ class Node:
     def build_plot(self):
         plt.plot(self.x_coord, self.y_coord, 'o', color = (0, 0, 1, 0.1))
         # plots the ending nodes
+        x_array = [self.x_coord]
+        # creates a list of x-coordinates this node has traveled
+        y_array = [self.y_coord]
+        # creates a list of y coordinates this node has traveled
+        cur_parent = self.parent
+        # the current parent node
+        while cur_parent is not None:
+            # plots the parent node
+            x_array.append(cur_parent.x_coord)
+            # adds the parent node's x-coordinate to the array
+            y_array.append(cur_parent.y_coord)
+            # adds the parent node's y-coordinate to the array
+            cur_parent = cur_parent.parent
+            # moves the plot one generation back
+        plt.plot(x_array, y_array, color = (0, 0, 0, 0.1))
+        # plots the path in its entirety
     
     def removed(self, other):
         if(self.gen != other.gen):
@@ -39,38 +54,36 @@ class Node:
         return self.gen - first_parent.gen - 1
         # returns how far removed this node and other are from each other
 
-    def change_node_angle_position(self):
+    def change_node_position(self):
         self.angle = random.randint(self.parent.angle - 85, self.parent.angle + 85)
         # sets this nodes angle to a new random value
-        self.x_coord = self.parent.x_coord + self.steps * math.cos(math.radians(self.angle))
+        self.x_coord = self.parent.x_coord + math.cos(math.radians(self.angle))
         # changes this node's x-coordinate based on the new angle
-        self.y_coord = self.parent.y_coord + self.steps * math.sin(math.radians(self.angle))
+        self.y_coord = self.parent.y_coord + math.sin(math.radians(self.angle))
         # changes this node's y-coordinate based on the new angle
-
-    def change_node_step_position(self):
-        self.steps += 1
-        self.x_coord = self.parent.x_coord + self.steps * math.cos(math.radians(self.angle))
-        self.y_coord = self.parent.y_coord + self.steps * math.sin(math.radians(self.angle))
 
     def change_num_kids(self, count):
         self.num_kids = 5 - count
 
-root = Node(None, 0, 0, 0, 1, 2, 0)
+root = Node(None, 0, 0, 0, 2, 0)
 x_coord_list = [0]
 y_coord_list = [0]
 
-def generate_node(parent_node, parent_x, parent_y, parent_gen, lower_bound, upper_bound, steps):
+def build_dot_plot():
+    plt.plot(x_coord_list, y_coord_list, 'o', color = (0, 0, 1, 0.1))
+
+def generate_node(parent_node, parent_x, parent_y, parent_gen, lower_bound, upper_bound):
     angle = random.randint(lower_bound, upper_bound)
     # gets an angle within the given bounds
-    x = parent_x + steps * math.cos(math.radians(angle))
+    x = parent_x + math.cos(math.radians(angle))
     # determines the x-coordinate of a point at the angle from the parent and distance 1
-    y = parent_y + steps * math.sin(math.radians(angle))
+    y = parent_y + math.sin(math.radians(angle))
     # determines the y-coordinate of a point at the angle from the parent and distance 1
-    if parent_gen < 29:
-        return Node(parent_node, angle, x, y, steps, 2, parent_gen + 1)
+    if parent_gen < 9:
+        return Node(parent_node, angle, x, y, 2, parent_gen + 1)
         # returns a node that will have two kids
     else:
-        return Node(parent_node, angle, x, y, steps, 0, parent_gen + 1)
+        return Node(parent_node, angle, x, y, 0, parent_gen + 1)
         # returns a node that will have zero kids, terminating the tree
 
 def build_gen(cur_gen):
@@ -92,9 +105,9 @@ def build_gen(cur_gen):
             # a list of all the parent's kids
             parent_gen = parent_node.gen
             # the parent's generation
-            if parent_gen < 9:
+            if parent_gen < 4:
                 for i in range(parent_node.num_kids):
-                    kid_node = generate_node(parent_node, parent_x, parent_y, parent_gen, 1, 360, 1)
+                    kid_node = generate_node(parent_node, parent_x, parent_y, parent_gen, 1, 360)
                     # generates a node with a random angle
                     x_coord_list.append(kid_node.x_coord)
                     y_coord_list.append(kid_node.y_coord)
@@ -105,16 +118,16 @@ def build_gen(cur_gen):
             else:
                 for i in range(parent_node.num_kids):
                     if len(next_gen) == 0 and i == 0:
-                        kid_node = generate_node(parent_node, parent_x, parent_y, parent_gen, parent_node.angle - 85, parent_node.angle + 85, 1)
+                        kid_node = generate_node(parent_node, parent_x, parent_y, parent_gen, parent_node.angle - 85, parent_node.angle + 85)
                         # generates a node with an angle within 170 degrees of the parent's angle
                         kid_x = kid_node.x_coord
                         kid_y = kid_node.y_coord
                         count = 0
                         for x, y in zip(x_coord_list, y_coord_list):
-                            if math.sqrt((kid_node.x_coord - x)**2 + (kid_node.y_coord - y)**2) < 5:
+                            if math.sqrt((kid_node.x_coord - x)**2 + (kid_node.y_coord - y)**2) < 2 * 0.9**kid_node.gen:
                                 count += 1
                                 if count == 5:
-                                    kid_node.change_node_step_position()
+                                    kid_node.change_node_position()
                                     count = 0
                                     break
                         loop = 0
@@ -124,90 +137,20 @@ def build_gen(cur_gen):
                             kid_y = kid_node.y_coord
                             # resets what the original y-coordinate of the node is
                             for x, y in zip(x_coord_list, y_coord_list):
-                                if math.sqrt((kid_node.x_coord - x)**2 + (kid_node.y_coord - y)**2) < 5:
+                                if math.sqrt((kid_node.x_coord - x)**2 + (kid_node.y_coord - y)**2) < 2 * 0.9**kid_node.gen:
                                     count += 1
                                     if count == 5:
-                                        kid_node.change_node_step_position()
+                                        kid_node.change_node_position()
                                         count = 0
                                         break
                                     # changes the position of the node to hopefully distance it from the cousin
                             loop += 1
                         if kid_node.num_kids != 0:
                             for x, y in zip(x_coord_list, y_coord_list):
-                                if math.sqrt((kid_node.x_coord - x)**2 + (kid_node.y_coord - y)**2) < 5:
+                                if math.sqrt((kid_node.x_coord - x)**2 + (kid_node.y_coord - y)**2) < 2 * 0.9**kid_node.gen:
                                     count += 1
-                            if count >= 5:
-                                kid_node.change_num_kids(4)
-                            else:
-                                kid_node.change_num_kids(count)
-                        x_coord_list.append(kid_node.x_coord)
-                        y_coord_list.append(kid_node.y_coord)
-                        parent_next.append(kid_node)
-                        # adds the node to its parent's list of kids
-                        next_gen.append(kid_node)
-                        # adds the node to the list of kids in this generation
-                        firstborn.append(kid_node)
-                    elif i == 0:
-                        kid_angle = random.randint(parent_angle - 85, parent_angle + 85)
-                        # generates an angle within 170 degrees of the parent's angle
-                        kid_x = parent_x + math.cos(math.radians(kid_angle))
-                        # determines the x-coordinate of a point at the angle from the parent and distance 1
-                        kid_y = parent_y + math.sin(math.radians(kid_angle))
-                        # determines the y-coordinate of a point at the angle from the parent and distance 1
-                        if parent_node.gen < 29:
-                            kid_node = Node(parent_node, kid_angle, kid_x, kid_y, 1, 2, parent_gen + 1)
-                            # generates a node that will have two kids
-                        else:
-                            kid_node = Node(parent_node, kid_angle, kid_x, kid_y, 1, 0, parent_gen + 1)
-                            # generates a node that will have zero kids, terminating the tree
-                        count = 0
-                        for x, y in zip(x_coord_list, y_coord_list):
-                            if math.sqrt((kid_node.x_coord - x)**2 + (kid_node.y_coord - y)**2) < 5:
-                                count += 1
-                                if count == 5:
-                                    kid_node.change_node_step_position()
-                                    count = 0
-                                    break
-                        loop = 0
-                        while (kid_x != kid_node.x_coord or kid_y != kid_node.y_coord) and loop < 5:
-                            kid_x = kid_node.x_coord
-                            # resets what the original x-coordinate of the node is
-                            kid_y = kid_node.y_coord
-                            # resets what the original y-coordinate of the node is
-                            for x, y in zip(x_coord_list, y_coord_list):
-                                if math.sqrt((kid_node.x_coord - x)**2 + (kid_node.y_coord - y)**2) < 5:
-                                    count += 1
-                                    if count == 5:
-                                        kid_node.change_node_step_position()
-                                        count = 0
-                                        break
-                                    # changes the position of the node to hopefully distance it from the cousin
-                            loop += 1
-                        for cousin_node in firstborn:
-                            # calculates the distance between the cousin and the node
-                            if math.sqrt((kid_node.x_coord - cousin_node.x_coord)**2 + (kid_node.y_coord - cousin_node.y_coord)**2) < 0.9**kid_node.removed(cousin_node):
-                                kid_node.change_node_angle_position()
-                                # changes the position of the node to hopefully distance it from the cousin
-                        loop = 0
-                        # runs the check five times
-                        while (kid_x != kid_node.x_coord or kid_y != kid_node.y_coord) and loop < 5:
-                            kid_x = kid_node.x_coord
-                            # resets what the original x-coordinate of the node is
-                            kid_y = kid_node.y_coord
-                            # resets what the original y-coordinate of the node is
-                            for cousin_node in firstborn:
-                                # calculates the distance between the cousin and the node
-                                if math.sqrt((kid_node.x_coord - cousin_node.x_coord)**2 + (kid_node.y_coord - cousin_node.y_coord)**2) < 0.9**kid_node.removed(cousin_node):
-                                    kid_node.change_node_angle_position()
-                                    # changes the position of the node to hopefully distance it from the cousin
-                            loop += 1
-                        if kid_node.num_kids != 0:
-                            for x, y in zip(x_coord_list, y_coord_list):
-                                if math.sqrt((kid_node.x_coord - x)**2 + (kid_node.y_coord - y)**2) < 5:
-                                    count += 1
-                            if count >= 5:
-                                kid_node.change_num_kids(4)
-                            else:
+                            print(count)
+                            if count < 5:
                                 kid_node.change_num_kids(count)
                         x_coord_list.append(kid_node.x_coord)
                         y_coord_list.append(kid_node.y_coord)
@@ -217,15 +160,46 @@ def build_gen(cur_gen):
                         # adds the node to the list of kids in this generation
                         firstborn.append(kid_node)
                     else:
-                        kid_node = generate_node(parent_node, parent_x, parent_y, parent_gen, parent_next[0].angle - 5, parent_next[0].angle + 5, parent_next[0].steps)
-                        # generates an angle within 10 degrees of the eldest sibling's angle
+                        kid_angle = random.randint(parent_angle - 85, parent_angle + 85)
+                        # generates an angle within 170 degrees of the parent's angle
+                        kid_x = parent_x + math.cos(math.radians(kid_angle))
+                        # determines the x-coordinate of a point at the angle from the parent and distance 1
+                        kid_y = parent_y + math.sin(math.radians(kid_angle))
+                        # determines the y-coordinate of a point at the angle from the parent and distance 1
+                        if parent_node.gen < 9:
+                            kid_node = Node(parent_node, kid_angle, kid_x, kid_y, 2, parent_gen + 1)
+                            # generates a node that will have two kids
+                        else:
+                            kid_node = Node(parent_node, kid_angle, kid_x, kid_y, 0, parent_gen + 1)
+                            # generates a node that will have zero kids, terminating the tree
+                        count = 0
+                        for x, y in zip(x_coord_list, y_coord_list):
+                            if math.sqrt((kid_node.x_coord - x)**2 + (kid_node.y_coord - y)**2) < 2 * 0.9**kid_node.gen:
+                                count += 1
+                                if count == 5:
+                                    kid_node.change_node_position()
+                                    count = 0
+                                    break
+                        loop = 0
+                        while (kid_x != kid_node.x_coord or kid_y != kid_node.y_coord) and loop < 5:
+                            kid_x = kid_node.x_coord
+                            # resets what the original x-coordinate of the node is
+                            kid_y = kid_node.y_coord
+                            # resets what the original y-coordinate of the node is
+                            for x, y in zip(x_coord_list, y_coord_list):
+                                if math.sqrt((kid_node.x_coord - x)**2 + (kid_node.y_coord - y)**2) < 2 * 0.9**kid_node.gen:
+                                    count += 1
+                                    if count == 5:
+                                        kid_node.change_node_position()
+                                        count = 0
+                                        break
+                                    # changes the position of the node to hopefully distance it from the cousin
+                            loop += 1
                         if kid_node.num_kids != 0:
                             for x, y in zip(x_coord_list, y_coord_list):
-                                if math.sqrt((kid_node.x_coord - x)**2 + (kid_node.y_coord - y)**2) < 5:
+                                if math.sqrt((kid_node.x_coord - x)**2 + (kid_node.y_coord - y)**2) < 2 * 0.9**kid_node.gen:
                                     count += 1
-                            if count >= 5:
-                                kid_node.change_num_kids(4)
-                            else:
+                            if count < 5:
                                 kid_node.change_num_kids(count)
                         x_coord_list.append(kid_node.x_coord)
                         y_coord_list.append(kid_node.y_coord)
@@ -233,13 +207,15 @@ def build_gen(cur_gen):
                         # adds the node to its parent's list of kids
                         next_gen.append(kid_node)
                         # adds the node to the list of kids in this generation
+                        firstborn.append(kid_node)
         print("Gen " + str(next_gen[0].gen) + " Built!")
         build_gen(next_gen)
         # starts the process again for the next generation
     else:
         for node in cur_gen:
             node.build_plot()
-            # plots the path the node took to get to the end of the tree
+        # build_dot_plot()    
+        # # plots the path the node took to get to the end of the tree
 
 build_gen([root])
 # builds and plots the tree
